fe390cb15ce138600d00840996b143bbd110e0c4
Matthew Cotton <matt@tensorworks.com.au>
2023-12-05 10:34:16 +1100

    Initial config

---
+++ lua/mcotton/init.lua
@@ -0,0 +1,4 @@
+require("mcotton.remap")
+require("mcotton.set")
+
+
---
+++ lua/mcotton/remap.lua
@@ -0,0 +1,44 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- highlight and move whole block up or down with J K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append line below to the end of this line with a space but leave the cursor
+vim.keymap.set("n", "J", "mzJ`z")
+-- jump half a page up or down leaving the curser centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- next/prev search match but in center of screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste but putting the replacement into the void register
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to the system clipboard rather than internal vim
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete into void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- format entire buffer?
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+-- quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- quick replace
+vim.keymap.set("n", "<leader>s", [[:%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
+
