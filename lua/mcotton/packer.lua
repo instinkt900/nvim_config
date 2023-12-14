cbc5591b29eb3fac6ae3efc70086977601b65e3b
Matthew Cotton <matt@tensorworks.com.au>
2023-12-15 09:59:07 +1100

    updated to harpoon2

--- lua/mcotton/packer.lua
+++ lua/mcotton/packer.lua
@@ -22,7 +22,11 @@ return require('packer').startup(function(use)
   })
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate' })
   use('nvim-treesitter/playground')
-  use('ThePrimeagen/harpoon')
+	use {
+	    "ThePrimeagen/harpoon",
+	    branch = "harpoon2",
+	    requires = { {"nvim-lua/plenary.nvim"} }
+	}
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
 
