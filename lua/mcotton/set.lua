a0ad7b0cd31fead0dceee54a6e1f8faab2e81783
instinkt900 <instinkt@gmail.com>
2024-03-06 11:28:57 +1100

    Adding header switch alt-o. Adding highlight on yank.

--- lua/mcotton/set.lua
+++ lua/mcotton/set.lua
@@ -34,3 +34,11 @@ vim.opt.colorcolumn = "100"
 
 vim.g.mapleader = " "
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
