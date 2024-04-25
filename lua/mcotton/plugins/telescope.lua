7428a2c4449c93bca072e5bd0a6f1984cef3dc78
Matthew Cotton <matt@tensorworks.com.au>
2024-04-25 18:17:19 +1000

    Fixing fzf build for windows

--- lua/mcotton/plugins/telescope.lua
+++ lua/mcotton/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     version = '0.1.x',
     dependencies = {
         { 'nvim-lua/plenary.nvim' },
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = "make" },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" },
         "folke/todo-comments.nvim",
     },
     config = function()
