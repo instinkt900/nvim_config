2b2a1729baed7303b0c348308b02619a06a423f3
Matthew Cotton <instinkt@gmail.com>
2026-08-14 18:46:35 +1000

    fixed treesitter

--- lua/mcotton/plugins/treesitter.lua
+++ lua/mcotton/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
     config = function()
         -- when this draws virtual text scrolling performance drops
